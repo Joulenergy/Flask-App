6c9cf20a1e48377dab9c2f28cbf27b66511a644b
Joulenergy <joulesisaunitofenergy@gmail.com>
2023-02-16 23:56:23 +0800

    froshims commit

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ def index():
     # but usually people just name=name and the first name is referring to the name we will use in the html
     return render_template("index.html")
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
-    name = request.args.get("name") 
-    # doing "name", "world" doesn't work to prevent hello, [blank] from happening
+    name = request.form.get("name", "world") 
+    # request.args for GET and request.form for POST
+    # doing "name", "world" may not work to prevent hello, [blank] from happening
     # since submit with name field empty will still have the /greet?name=
     # and will not just be /greet
     return render_template("greet.html", name=name)
